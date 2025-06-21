da4c77193b9b6b37237fcfd4449b6630775563f1
ArevaloDev <arevalodaniel999@gmail.com>
2025-06-21 09:33:45 -0600

    agregando bootstrap

--- movie_app/src/app/movies/components/moviedetails/moviedetails.component.ts
+++ movie_app/src/app/movies/components/moviedetails/moviedetails.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-moviedetails',
   templateUrl: './moviedetails.component.html',
   styleUrl: './moviedetails.component.css'
 })
-export class MoviedetailsComponent {
+export class MoviedetailsComponent implements OnInit {
+  constructor(private route:ActivatedRoute){}
+
+  ngOnInit(): void {
+    const id:number = Number(this.route.snapshot.paramMap.get('id'));
+    console.log(id);
+
+    if(id){
+
+    }
+  }
 
 }
