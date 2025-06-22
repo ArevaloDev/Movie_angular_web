965c9d372e0a51f7f8361773b5f92d825763969d
ArevaloDev <arevalodaniel999@gmail.com>
2025-06-21 18:23:24 -0600

    refactorizando home component

--- movie_app/src/app/movies/pages/home/home.component.ts
+++ movie_app/src/app/movies/pages/home/home.component.ts
@@ -1,66 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Movies } from '../../interfaces/movies.interface';
 import { MovieService } from '../../services/movie.service';
 import { Router } from '@angular/router';
-import { finalize } from 'rxjs';
+import { finalize, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit, OnDestroy {
   public popularMovies: Movies[] = [];
   public currentPage: number = 1;
   public totalPages: number = 0;
-  public isLoading:boolean = false;
-  constructor(
-    private movieService: MovieService,
-    private router:Router
-  ) {}
+  public isLoading: boolean = false;
+  private searchSub!: Subscription;
+  public errorMessage: string = '';
+
+  constructor(private movieService: MovieService, private router: Router) {}
 
   ngOnInit(): void {
-    this.loadMovies(this.currentPage);
+    this.isLoading = true;
+    this.searchSub = this.movieService.searchjResult$.subscribe((results) => {
+      if (results.length > 0) {
+        this.popularMovies = results;
+        this.isLoading = false;
+      } else {
+        this.loadMovies(this.currentPage);
+      }
+    });
+  }
+  ngOnDestroy(): void {
+    this.searchSub.unsubscribe();
   }
 
-  loadMovies = (page:number): void => {
+  loadMovies = (page: number): void => {
     this.isLoading = true;
-    this.movieService.getPopularMovies(page).pipe(
-      finalize(() => {
-        this.isLoading = false;
-      })
-    ).subscribe((response) => {
-      console.log(response);
-      this.popularMovies = response.results;
-      this.currentPage = response.page;
-      this.totalPages = response.total_pages;
-    }, (error) => {
-      console.log('error ak cargar peliculas', error);
-
-    })
+    this.movieService
+      .getPopularMovies(page)
+      .pipe(finalize(() => (this.isLoading = false)))
+      .subscribe(
+        {
+          next: (response) => {
+            console.log(response);
+            this.popularMovies = response.results;
+            this.currentPage = response.page;
+            this.totalPages = response.total_pages;
+          },
+          error: () => {
+                this.errorMessage = 'Ocurrió un error al cargar las películas. Inténtalo de nuevo más tarde.'
+          },
+        }
+      );
   };
 
-  goToPage = (page:number):void => {
-      if(page > 0 && page <= this.totalPages){
-        this.loadMovies(this.currentPage + 1);
-      }
-  }
-
   nextPage = (): void => {
-    if ((this.currentPage < this.totalPages)) {
+    if (this.currentPage < this.totalPages) {
       this.loadMovies(this.currentPage + 1);
     }
   };
 
-  prevPage = ():void => {
-    if(this.currentPage > 1){
+  prevPage = (): void => {
+    if (this.currentPage > 1) {
       this.loadMovies(this.currentPage - 1);
     }
-  }
+  };
 
-  goToDetail = (id:number):void =>  {
+  goToDetail = (id: number): void => {
     console.log(id);
-    this.router.navigate(['/movies/movie', id])
-  }
-
+    this.router.navigate(['/movies/movie', id]);
+  };
 }
