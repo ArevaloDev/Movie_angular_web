893d34220ffe461ed1a7af9a96dd684a6f630951
ArevaloDev <arevalodaniel999@gmail.com>
2025-06-21 18:23:04 -0600

    aplicando refactorizacion de codigo

--- movie_app/src/app/movies/components/moviedetails/moviedetails.component.ts
+++ movie_app/src/app/movies/components/moviedetails/moviedetails.component.ts
@@ -1,30 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { finalize, of, switchMap } from 'rxjs';
+import { finalize, Observable, of, Subscription, switchMap } from 'rxjs';
 import { MovieService } from '../../services/movie.service';
-import { Movie } from '../../interfaces/movies.interface';
+import { Movie, Movies } from '../../interfaces/movies.interface';
 
 @Component({
   selector: 'app-moviedetails',
   templateUrl: './moviedetails.component.html',
   styleUrl: './moviedetails.component.css',
 })
-export class MoviedetailsComponent implements OnInit {
+export class MoviedetailsComponent implements OnInit, OnDestroy {
   public movie!: Movie;
   public isLoading: boolean = false;
+  public errorMessage: string = '';
+  public subscription!: Subscription;
   constructor(
     private route: ActivatedRoute,
     private movieService: MovieService
   ) {}
 
   ngOnInit(): void {
+    this.getMovieDetails();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe;
+  }
+
+  getMovieDetails = () => {
     this.isLoading = true;
-    this.route.paramMap
+    this.subscription = this.route.paramMap
       .pipe(
         switchMap((params) => {
           const id = Number(params.get('id'));
           if (isNaN(id)) {
-            console.log('Id invalido');
             return of();
           }
           return this.movieService.getMovieById(id).pipe(
@@ -32,11 +41,18 @@ export class MoviedetailsComponent implements OnInit {
               this.isLoading = false;
             })
           );
-        })
+        }),
+        finalize(() => (this.isLoading = false))
       )
-      .subscribe((response) => {
-        console.log(response);
-        this.movie = response;
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+          this.movie = response;
+        },
+        error: () => {
+          this.errorMessage =
+            'Ocurrió un error al cargar las películas. Inténtalo de nuevo más tarde.';
+        },
       });
-  }
+  };
 }
