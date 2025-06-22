fbe5cf817a8d8fc7f2cf2a73a6820e87ce346997
ArevaloDev <arevalodaniel999@gmail.com>
2025-06-21 18:23:45 -0600

    agregando componente global para errorres de apis

--- movie_app/src/app/shared/shared.module.ts
+++ movie_app/src/app/shared/shared.module.ts
@@ -2,19 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { SearchbarComponent } from './components/searchbar/searchbar.component';
+import { RouterModule } from '@angular/router';
+import {  ReactiveFormsModule } from '@angular/forms';
+import { ErrorMessageComponent } from './components/error-message/error-message.component';
 
 
 
 @NgModule({
   declarations: [
-      SearchbarComponent
+      SearchbarComponent,
+      ErrorMessageComponent
   ],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule,
+    ReactiveFormsModule
   ],
   exports:[
     SearchbarComponent,
+    ErrorMessageComponent
   ]
 })
 export class SharedModule { }
