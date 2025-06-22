42c52b3d8ed88e2eef75ede98144a275b7ce8e32
ArevaloDev <arevalodaniel999@gmail.com>
2025-06-21 18:22:25 -0600

    agregando formulario para busqueda de pelicula y consumiendo servicio

--- movie_app/src/app/shared/components/searchbar/searchbar.component.ts
+++ movie_app/src/app/shared/components/searchbar/searchbar.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MovieService } from '../../../movies/services/movie.service';
 
 @Component({
   selector: 'app-searchbar',
   templateUrl: './searchbar.component.html',
   styleUrl: './searchbar.component.css'
 })
-export class SearchbarComponent {
+export class SearchbarComponent implements OnInit {
 
+  public form!:FormGroup;
+  constructor(
+    private fb:FormBuilder,
+    private movieService:MovieService
+  ){
+     this.form  = this.fb.group({
+        movie: ['']
+      })
+  }
+  ngOnInit(): void {
+
+  }
+  searchMovie = () => {
+    const value = this.form.get('movie')?.value;
+    if(value.length === 0) return;
+    this.movieService.searchMovies(value);
+
+  }
 }
